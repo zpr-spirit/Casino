9570925ad1bfec302832eba266980e71de1c42d0
ryan.zhang <ryan.zhang@insilico.ai>
2025-09-22 01:56:47 +0000

    Remove node_modules from repository

--- full_house/node_modules/element-plus/es/components/alert/index.mjs
+++
@@ -1,8 +0,0 @@
-import Alert from './src/alert.mjs';
-export { alertEffects, alertEmits, alertProps } from './src/alert2.mjs';
-import { withInstall } from '../../utils/vue/install.mjs';
-
-const ElAlert = withInstall(Alert);
-
-export { ElAlert, ElAlert as default };
-//# sourceMappingURL=index.mjs.map
